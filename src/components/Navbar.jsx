1a917376f1c074c3343a9b66d4cf027db385aec7
Posterity147 <yusufriliwan007@gmail.com>
2025-08-24 09:14:14 +0100

    update to vercel.json and the header name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             />
           </Link>
           <span className="text-white text-lg font-medium italic tracking-wide">
-            SecureTrustInsure
+            SecureTrustsInsure
           </span>
         </div>
 
